ee43d19ac002257f6bcf3e201431f51d87bd4609
saathvik-345 <shanthimani278@gmail.com>
2020-02-24 20:51:52 +0530

    trail

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
+var gameState="onSling";
 var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
@@ -69,12 +70,16 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+   
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState="launched";
 }
 
 function keyPressed(){
